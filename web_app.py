b4532c15484867f04cf18e468839f251bf04b9ec
cdsa1230126-ai <cdsa1230126@gn.iwasaki.ac.jp>
2026-04-17 10:58:46 +0900

    Add files via upload

--- web_app.py
+++ web_app.py
@@ -3,19 +3,18 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import json
 
-# Firebase初期化
 if not firebase_admin._apps:
     try:
-        # Secretsから文字列としてJSONを取得
+        # Secretsから文字列を取得
         info_str = st.secrets["firebase"]["info"]
         
-        # 文字列を辞書オブジェクトに変換
+        # 文字列を辞書(dict)に変換
         info_dict = json.loads(info_str)
         
-        # 秘密鍵の改行文字が正しく解釈されるよう念のため処理
-        info_dict["private_key"] = info_dict["private_key"].replace("\\n", "\n")
+        # 秘密鍵内の改行（\n）が、文字列として入っている場合を考慮して置換
+        if "private_key" in info_dict:
+            info_dict["private_key"] = info_dict["private_key"].replace("\\n", "\n")
         
-        # 認証
         cred = credentials.Certificate(info_dict)
         firebase_admin.initialize_app(cred)
         
@@ -24,4 +23,4 @@ if not firebase_admin._apps:
         st.stop()
 
 db = firestore.client()
-st.success("🎯 ついに接続成功しました！おめでとうございます！")
+st.success("🎯 ついに接続成功です！")
